5eedc278c3e7d418114fa855c196494c9222a3c8
Patrick Bozeman <pbozeman@gmail.com>
2024-06-12 18:22:13 -0700

    [verilog] add flake and basic setup

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Verilog dev env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gtkwave
+            yosys
+            verilog
+          ];
+        };
+      }
+    );
+}
