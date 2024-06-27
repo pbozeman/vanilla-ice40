6b42ed4b48f9349872050b8fc388fd673df488aa
Patrick Bozeman <pbozeman@gmail.com>
2024-06-26 18:26:28 -0700

    [blinky] blinky_all does all pmod pins

--- examples/blinky_pmod/blinky_all_top.v
+++ examples/blinky_pmod/blinky_all_top.v
@@ -1,17 +1,15 @@
-`define PINS CD_PINS
-
 module blinky_all_top #(
-    parameter NUM_PINS = 8
+    parameter NUM_PINS = 96
 ) (
     input wire clk_i,
-    output wire [NUM_PINS-1:0] `PINS
+    output wire [NUM_PINS-1:0] PMOD_PINS
 );
 
   pin_walker #(
       .NUM_PINS(NUM_PINS)
   ) uut (
       .clk_i (clk_i),
-      .pins_o(`PINS)
+      .pins_o(PMOD_PINS)
   );
 
 endmodule
