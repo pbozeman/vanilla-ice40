3e29094e90b8ceaa721211af2d63204861e28597
Patrick Bozeman <pbozeman@gmail.com>
2024-06-29 14:06:37 -0700

    [sram] comment out while integrating pnr

--- examples/sram_test/sram_tester_top.v
+++ examples/sram_test/sram_tester_top.v
@@ -4,55 +4,60 @@
 // avoid undeclared symbols
 `default_nettype none
 
-module sram_tester_top #(
-    parameter integer ADDR_BITS = 20,
-    parameter integer DATA_BITS = 16
-) (
-    input  wire CLK,
-    input  wire reset,
-    output wire test_done,
-    output wire LED1,
-    output wire LED2,
-
-    // sram pins
-    output wire [ADDR_BITS-1:0] SRAM_ADDR_BUS,
-    inout wire [DATA_BITS-1:0] SRAM_DATA_BUS,
-    output wire we_n,
-    output wire oe_n,
-    output wire ce_n
+module sram_tester_top (
+    input wire CLK
 );
-
-  // Internal signals
-  wire [ADDR_BITS-1:0] addr;
-  wire [DATA_BITS-1:0] data_write;
-  wire [DATA_BITS-1:0] data_read;
-  wire read_only;
-  wire test_pass;
-
-  // Instantiate the sram_tester
-  sram_tester #(
-      .ADDR_BITS(ADDR_BITS),
-      .DATA_BITS(DATA_BITS)
-  ) tester (
-      .clk(CLK),
-      .reset(reset),
-      .test_done(test_done),
-      .test_pass(test_pass),
-      .read_only(read_only),
-      .addr(addr),
-      .data_write(data_write),
-      .data_read(data_read),
-      .addr_bus(SRAM_ADDR_BUS),
-      .data_bus(SRAM_DATA_BUS),
-      .we_n(we_n),
-      .oe_n(oe_n),
-      .ce_n(ce_n)
-  );
-
-  // LED1 blinks every addr reset (too fast to see, use a scope)
-  assign LED1 = (addr == {ADDR_BITS{1'b0}});
-
-  // LED2 is success
-  assign LED2 = test_pass;
-
 endmodule
+
+//module sram_tester_top #(
+//    parameter integer ADDR_BITS = 20,
+//    parameter integer DATA_BITS = 16
+//) (
+//    input  wire CLK,
+//    input  wire reset,
+//    output wire test_done,
+//    output wire LED1,
+//    output wire LED2,
+//
+//    // sram pins
+//    output wire [ADDR_BITS-1:0] SRAM_ADDR_BUS,
+//    inout wire [DATA_BITS-1:0] SRAM_DATA_BUS,
+//    output wire we_n,
+//    output wire oe_n,
+//    output wire ce_n
+//);
+//
+//  // Internal signals
+//  wire [ADDR_BITS-1:0] addr;
+//  wire [DATA_BITS-1:0] data_write;
+//  wire [DATA_BITS-1:0] data_read;
+//  wire read_only;
+//  wire test_pass;
+//
+//  // Instantiate the sram_tester
+//  sram_tester #(
+//      .ADDR_BITS(ADDR_BITS),
+//      .DATA_BITS(DATA_BITS)
+//  ) tester (
+//      .clk(CLK),
+//      .reset(reset),
+//      .test_done(test_done),
+//      .test_pass(test_pass),
+//      .read_only(read_only),
+//      .addr(addr),
+//      .data_write(data_write),
+//      .data_read(data_read),
+//      .addr_bus(SRAM_ADDR_BUS),
+//      .data_bus(SRAM_DATA_BUS),
+//      .we_n(we_n),
+//      .oe_n(oe_n),
+//      .ce_n(ce_n)
+//  );
+//
+//  // LED1 blinks every addr reset (too fast to see, use a scope)
+//  assign LED1 = (addr == {ADDR_BITS{1'b0}});
+//
+//  // LED2 is success
+//  assign LED2 = test_pass;
+//
+//endmodule
