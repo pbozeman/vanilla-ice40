be5b7ef678301f2733c71926b619045496aba470
Patrick Bozeman <pbozeman@gmail.com>
2024-08-30 13:36:47 -0700

    [test] WAIT_FOR_SIGNAL assertion

--- examples/test/testing.v
+++ examples/test/testing.v
@@ -16,4 +16,16 @@
      $dumpvars(0, mod);                       \
    end
 
+`define WAIT_FOR_SIGNAL(signal)      \
+  begin : wait_for_sig_`__LINE__     \
+    reg [8:0] cnt;                   \
+    cnt = 0;                         \
+    while (!(signal)) begin          \
+      @(posedge axi_aclk);           \
+      cnt = cnt + 1;                 \
+      `ASSERT(cnt < 10);             \
+    end                              \
+    `ASSERT((signal) === 1'b1);      \
+  end
+
 `endif
