11f26a181a8c1d1ca04e67b8b053368bd2737724
Patrick Bozeman <pbozeman@gmail.com>
2024-10-07 14:27:48 -0700

    [vga_sram] pattern gen using axi sram

---
+++ examples/test/vga_sram_tb.v
@@ -0,0 +1,135 @@
+
+`include "testing.v"
+`include "sram_model.v"
+
+`include "axi_sram_controller.v"
+`include "vga_sram_pattern_generator.v"
+
+// This is not intended to be a full test. This is just to see some wave forms
+// in the simulator.
+
+module vga_sram_tb;
+
+  parameter AXI_ADDR_WIDTH = 20;
+  parameter AXI_DATA_WIDTH = 16;
+
+  reg                               clk;
+  reg                               reset;
+
+  // AXI-Lite Write Address Channel
+  wire [        AXI_ADDR_WIDTH-1:0] s_axi_awaddr;
+  wire                              s_axi_awvalid;
+  wire                              s_axi_awready;
+
+  // AXI-Lite Write Data Channel
+  wire [        AXI_DATA_WIDTH-1:0] s_axi_wdata;
+  wire [((AXI_DATA_WIDTH+7)/8)-1:0] s_axi_wstrb;
+  wire                              s_axi_wvalid;
+  wire                              s_axi_wready;
+
+  // AXI-Lite Write Response Channel
+  wire [                       1:0] s_axi_bresp;
+  wire                              s_axi_bvalid;
+  wire                              s_axi_bready;
+
+  // AXI-Lite Read Address Channel
+  wire [        AXI_ADDR_WIDTH-1:0] s_axi_araddr;
+  wire                              s_axi_arvalid;
+  wire                              s_axi_arready;
+
+  // AXI-Lite Read Data Channel
+  wire [        AXI_DATA_WIDTH-1:0] s_axi_rdata;
+  wire [                       1:0] s_axi_rresp;
+  wire                              s_axi_rvalid;
+  wire                              s_axi_rready;
+
+  // SRAM Interface
+  wire [        AXI_ADDR_WIDTH-1:0] sram_addr;
+  wire [        AXI_DATA_WIDTH-1:0] sram_data;
+  wire                              sram_we_n;
+  wire                              sram_oe_n;
+  wire                              sram_ce_n;
+
+  vga_sram_pattern_generator #(
+      .AXI_ADDR_WIDTH(AXI_ADDR_WIDTH),
+      .AXI_DATA_WIDTH(AXI_DATA_WIDTH)
+  ) pattern (
+      .clk  (clk),
+      .reset(reset),
+
+      .s_axi_awaddr (s_axi_awaddr),
+      .s_axi_awvalid(s_axi_awvalid),
+      .s_axi_awready(s_axi_awready),
+
+      .s_axi_wdata (s_axi_wdata),
+      .s_axi_wstrb (s_axi_wstrb),
+      .s_axi_wvalid(s_axi_wvalid),
+      .s_axi_wready(s_axi_wready),
+
+      .s_axi_bresp (s_axi_bresp),
+      .s_axi_bvalid(s_axi_bvalid),
+      .s_axi_bready(s_axi_bready)
+  );
+
+  axi_sram_controller #(
+      .AXI_ADDR_WIDTH(AXI_ADDR_WIDTH),
+      .AXI_DATA_WIDTH(AXI_DATA_WIDTH)
+  ) ctrl (
+      .axi_aclk(clk),
+      .axi_aresetn(~reset),
+      .s_axi_awaddr(s_axi_awaddr),
+      .s_axi_awvalid(s_axi_awvalid),
+      .s_axi_awready(s_axi_awready),
+      .s_axi_wdata(s_axi_wdata),
+      .s_axi_wstrb(s_axi_wstrb),
+      .s_axi_wvalid(s_axi_wvalid),
+      .s_axi_wready(s_axi_wready),
+      .s_axi_bresp(s_axi_bresp),
+      .s_axi_bvalid(s_axi_bvalid),
+      .s_axi_bready(s_axi_bready),
+      .s_axi_araddr(s_axi_araddr),
+      .s_axi_arvalid(s_axi_arvalid),
+      .s_axi_arready(s_axi_arready),
+      .s_axi_rdata(s_axi_rdata),
+      .s_axi_rresp(s_axi_rresp),
+      .s_axi_rvalid(s_axi_rvalid),
+      .s_axi_rready(s_axi_rready),
+      .sram_addr(sram_addr),
+      .sram_data(sram_data),
+      .sram_we_n(sram_we_n),
+      .sram_oe_n(sram_oe_n),
+      .sram_ce_n(sram_ce_n)
+  );
+
+
+  // Instantiate the mocked SRAM model
+  sram_model #(
+      .ADDR_BITS(AXI_ADDR_WIDTH),
+      .DATA_BITS(AXI_DATA_WIDTH)
+  ) sram (
+      .we_n(sram_we_n),
+      .oe_n(sram_oe_n),
+      .ce_n(sram_ce_n),
+      .addr(sram_addr),
+      .data_io(sram_data)
+  );
+
+  // Clock generation
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+
+  `TEST_SETUP(vga_sram_tb);
+
+  // Test procedure
+  initial begin
+    reset = 1;
+    repeat (10) @(posedge clk);
+    reset = 0;
+
+    repeat (3 * 640 * 480) @(posedge clk);
+    $finish;
+  end
+
+endmodule
---
+++ examples/vga_sram/vga_sram_pattern_generator.v
@@ -0,0 +1,154 @@
+`ifndef VGA_SRAM_PATTERN_GENERATOR_V
+`define VGA_SRAM_PATTERN_GENERATOR_V
+
+`include "directives.v"
+
+module vga_sram_pattern_generator #(
+    parameter AXI_ADDR_WIDTH = 20,
+    parameter AXI_DATA_WIDTH = 16
+) (
+    input wire clk,
+    input wire reset,
+
+    // SRAM AXI-Lite Write Address Channel
+    output reg  [AXI_ADDR_WIDTH-1:0] s_axi_awaddr,
+    output reg                       s_axi_awvalid,
+    input  wire                      s_axi_awready,
+
+    // SRAM AXI-Lite Write Data Channel
+    output reg  [        AXI_DATA_WIDTH-1:0] s_axi_wdata,
+    output reg  [((AXI_DATA_WIDTH+7)/8)-1:0] s_axi_wstrb,
+    output reg                               s_axi_wvalid,
+    input  wire                              s_axi_wready,
+
+    // SRAM AXI-Lite Write Response Channel
+    input  wire [1:0] s_axi_bresp,
+    input  wire       s_axi_bvalid,
+    output reg        s_axi_bready
+);
+
+  // The "state" management of this state machine is kinda overkill
+  // but it's mentally convenient to have the style match the other
+  // axi clients.
+
+  // State definitions
+  localparam IDLE = 1'b0;
+  localparam WRITING = 1'b1;
+
+  // State and next state registers
+  reg state = IDLE;
+  reg next_state;
+
+  // Write controls
+  reg write_start;
+  wire write_done;
+
+  // vga and mem positions
+  reg [9:0] column = 0;
+  reg [9:0] row = 0;
+  wire [AXI_ADDR_WIDTH-1:0] addr;
+  wire [AXI_DATA_WIDTH-1:0] data;
+
+  wire pattern_done;
+  assign pattern_done = (row == 479 && column == 639);
+
+  // state machine
+  always @(*) begin
+    next_state  = state;
+    write_start = 1'b0;
+
+    if (!reset) begin
+      case (state)
+        IDLE: begin
+          if (!pattern_done) begin
+            write_start = 1'b1;
+            next_state  = WRITING;
+          end
+        end
+
+        WRITING: begin
+          if (write_done) begin
+            if (!pattern_done) begin
+              write_start = 1'b1;
+            end else begin
+              next_state = IDLE;
+            end
+          end
+        end
+      endcase
+    end
+  end
+
+  // state registration
+  always @(posedge clk or posedge reset) begin
+    if (reset) begin
+      state <= IDLE;
+    end else begin
+      state <= next_state;
+    end
+  end
+
+  // row/column
+  always @(posedge clk or posedge reset) begin
+    if (reset) begin
+      column <= 0;
+      row <= 0;
+    end else begin
+      if (!pattern_done) begin
+        if (write_done) begin
+          if (column < 640) begin
+            column <= column + 1;
+          end else begin
+            column <= 0;
+            row <= row + 1;
+          end
+        end
+      end
+    end
+  end
+
+  //
+  // AXI write
+  //
+  assign write_done = (s_axi_bready && s_axi_bvalid);
+
+  always @(posedge clk or posedge reset) begin
+    if (reset) begin
+      s_axi_awvalid <= 1'b0;
+      s_axi_wvalid  <= 1'b0;
+      s_axi_bready  <= 1'b0;
+    end else begin
+      // We're always ready for a response
+      s_axi_bready <= 1'b1;
+
+      // kick off a write, or wait to de-assert valid
+      if (write_start) begin
+        s_axi_awaddr  <= addr;
+        s_axi_wdata   <= data;
+        s_axi_awvalid <= 1'b1;
+        s_axi_wvalid  <= 1'b1;
+      end else begin
+        if (s_axi_awready && s_axi_awvalid) begin
+          s_axi_awvalid <= 1'b0;
+        end
+
+        if (s_axi_wready && s_axi_wvalid) begin
+          s_axi_wvalid <= 1'b0;
+        end
+      end
+    end
+  end
+
+  assign addr = (row * 640) + column;
+
+  assign data[15:12] = (row < 480 && column < 213) ? column : 4'b0000;
+  assign data[11:8] = (row < 480 && column >= 213 && column < 426) ? column : 4'b0000;
+  assign data[7:4] = (row < 480 && column >= 426 && column < 640) ? column : 4'b0000;
+  // assign data[15:12] = (row < 480 && column < 213) ? 4'b1111 : 4'b0000;
+  // assign data[11:8] = (row < 480 && column >= 213 && column < 426) ? 4'b1111 : 4'b0000;
+  // assign data[7:4] = (row < 480 && column >= 426 && column < 640) ? 4'b1111 : 4'b0000;
+  assign data[3:0] = 4'b0000;
+
+endmodule
+
+`endif
