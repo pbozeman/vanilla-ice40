106331adf2d58f8de00467c98adbd8591b84b65e
Patrick Bozeman <pbozeman@gmail.com>
2024-11-01 16:25:26 -0700

    [vga] add 800x600 mode and bad 1024 note
    The adc vga demo does not meet timing at 800x600 with nextpnr.

--- examples/lib/vga_mode.v
+++ examples/lib/vga_mode.v
@@ -5,6 +5,9 @@
 
 // verilog_format: off
 `ifdef VGA_MODE_1024_768_60
+  // the pixel clock is too fast for sram, as the sram is currently used
+  `include "VGA_MODE_1024_768_60 is broken (consider this an error directive)"
+
   // icepll -i 100 -o 65
   `define VGA_MODE_PLL_DIVR (4'd4)
   `define VGA_MODE_PLL_DIVF (7'd51)
@@ -25,6 +28,28 @@
   `define VGA_MODE_V_WHOLE_FRAME  806
 
   `define VGA_MODE_TB_PIXEL_CLK 7.69
+`else
+`ifdef VGA_MODE_800_600_60
+  // icepll -i 100 -o 40
+  `define VGA_MODE_PLL_DIVR (4'd4)
+  `define VGA_MODE_PLL_DIVF (7'd31)
+  `define VGA_MODE_PLL_DIVQ (3'd4)
+  `define VGA_MODE_PLL_FILTER_RANGE (3'd2)
+
+  // http://www.tinyvga.com/vga-timing/800x600@60Hz
+  `define VGA_MODE_H_VISIBLE      800
+  `define VGA_MODE_H_FRONT_PORCH  40
+  `define VGA_MODE_H_SYNC_PULSE   128
+  `define VGA_MODE_H_BACK_PORCH   88
+  `define VGA_MODE_H_WHOLE_LINE   1056
+
+  `define VGA_MODE_V_VISIBLE      600
+  `define VGA_MODE_V_FRONT_PORCH  1
+  `define VGA_MODE_V_SYNC_PULSE   4
+  `define VGA_MODE_V_BACK_PORCH   23
+  `define VGA_MODE_V_WHOLE_FRAME  628
+
+  `define VGA_MODE_TB_PIXEL_CLK 12.5
 `else
 `ifdef VGA_MODE_640_480_60
   // icepll -i 100 -o 25
@@ -51,7 +76,7 @@
   // There's no `error, so this will have to do
   `include "bad or missing VGA_MODE_ define (consider this an error directive)"
 `endif
-
+`endif
 `endif
 // verilog_format: on
 
