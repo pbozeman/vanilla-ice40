eb9e5308e17ed03bc1de6ef8983918d259396596
Patrick Bozeman <pbozeman@gmail.com>
2024-12-20 14:01:00 -0800

    [lib] minor iter timing optimization

--- rtl/lib/iter.sv
+++ rtl/lib/iter.sv
@@ -9,6 +9,7 @@
 // still jump over the WIDTH. (If this ever becomes needed, create a comb
 // to compute next_val and add next_val < val to the last check as a check
 // for overflow)
+
 module iter #(
     parameter WIDTH   = 4,
     parameter INC_VAL = 1
@@ -21,16 +22,19 @@ module iter #(
     output logic [WIDTH-1:0] val,
     output logic             last
 );
+  logic [WIDTH-1:0] next_val;
   logic [WIDTH-1:0] max_val_r;
+
+  assign next_val = val + INC_VAL;
 
   always_ff @(posedge clk) begin
     if (init) begin
       val       <= init_val;
       max_val_r <= max_val;
       last      <= (init_val == max_val);
-    end else if (!last && inc) begin
-      val  <= val + INC_VAL;
-      last <= ((val + INC_VAL) == max_val_r);
+    end else if (inc && !last) begin
+      val  <= next_val;
+      last <= (next_val == max_val_r);
     end
   end
 endmodule
